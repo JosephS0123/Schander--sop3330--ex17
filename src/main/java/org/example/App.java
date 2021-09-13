6dd338234ca2f9cbe747598fa2b4dfd1eb0271c2
Joseph <JosephSchander@gmail.com>
2021-09-12 20:16:13 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        DecimalFormat DF = new DecimalFormat("##.##");
+        Scanner uInput = new Scanner(System.in);
+        System.out.println( "Are you male or female? enter 1 for male and 2 for female" );
+        byte MorF = uInput.nextByte();
+        System.out.println("How many ounces of alcohol did you drink?");
+        int Ounces = uInput.nextInt();
+        System.out.println("What is your weight in pounds?");
+        int Weight = uInput.nextInt();
+        System.out.println("How many hours has it been since your last drink?");
+        int Hours = uInput.nextInt();
+        double MorF2 = (MorF==1)?.73:.66;
+        Double BAC = (Ounces *5.14/Weight*MorF2)-.015 * Hours;
+        System.out.println("Your BAC is "+BAC);
+        if(BAC>.08){
+            System.out.println("You are not legally allowed to drive");
+        }
+        else {
+            System.out.println("You are legally allowed to drive");
+        }
+
+    }
+}
